dd66dc8c6cb862782dbab8aeefb8e7e78d3020d3
Sharkeee <sharkmen1996@gmail.com>
2018-04-01 21:23:26 +0200

    ciag dalszy

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        ITelephone sharkPhone;
-        sharkPhone = new DeskPhone(790233176);
-        
+
+        int[] myIntegers = getInteregers(5);
+        printArray(myIntegers);
+
+
+    }
+
+    public static int[] getInteregers(int number) {
+        System.out.println("Enter " + number + " values:\r");
+        int[] values = new int[number];
+        for (int i = 0; i < number; i++) {
+            values[i] = scanner.nextInt();
+        }
+        return values;
+    }
+
+    public static void printArray(int[] array) {
+        System.out.println("Your array is:\r");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+        public static int[] sortArray ( int[] array){
+            
+        }
     }
 }
